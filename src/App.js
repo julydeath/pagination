6a8d7de8b55bae891a289e2723ae3dc521666982
julydeath <manojkarajada.mk@gmail.com>
2023-04-27 19:25:29 +0530

    final : pagination project created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+
+  console.log(products);
+
+  const handleClick = (e) => {
+    setPage(e.target.value);
+  };
+
+  const fetchData = () => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.slice(page * 10 - 10, page * 10).map((li) => (
+        <span key={li.id}>
+          <p>{li.title}</p>
+          <img src={li.thumbnail} alt="mame" />
+        </span>
+      ))}
+      <div>
+        {[...Array(products.length / 10)].map((li, index) => (
+          <button key={index} value={index + 1} onClick={(e) => handleClick(e)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
